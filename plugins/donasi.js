cf976af52c1ef2e17cff7d2e8ca6dc4e1a0d6b41
D'anang.W.S™✓ <98865918+Danangws@users.noreply.github.com>
2022-04-09 18:36:06 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,6 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat [085715590172]
-│ • Gopay [085715590172]
-╰────
-╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6285715590172
-╰────
-https://chat.whatsapp.com/I93WyQqfnA9KgZ6L6XdFPq
+
+
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
